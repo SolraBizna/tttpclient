14918cdba5bc08eb8af137f8e75412a13fc1b0d8
Solra Bizna <solra@bizna.name>
2016-03-29 13:14:55 -0600

    Copyright bump

--- src/fshader.glsl
+++ src/fshader.glsl
@@ -1,7 +1,7 @@
 // -*- c -*-
 
 /*
-  Copyright (C) 2015 Solra Bizna
+  Copyright (C) 2015-2016 Solra Bizna
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
--- src/glsl2h.lua
+++ src/glsl2h.lua
@@ -1,5 +1,5 @@
 --[[
-  Copyright (C) 2015 Solra Bizna
+  Copyright (C) 2015-2016 Solra Bizna
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
